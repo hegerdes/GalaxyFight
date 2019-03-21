33be811924b268d907902a800087015e128f1c63
lkreienbrink <lkreienbrink@uni-osnabrueck.de>
2019-03-21 11:00:56 +0100

    LoadingScreen laeuft

--- view/loadingscreen.cpp
+++ view/loadingscreen.cpp
@@ -10,7 +10,13 @@ LoadingScreen::LoadingScreen(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QMovie *movie = new QMovie("models/loading.gif");
+
+    //background-picture
+    QPixmap pic("models/start.jpg");
+    ui->picLabel->setPixmap(pic);
+
+    //loading-animation as a gif
+    QMovie *movie = new QMovie("models/laden4.gif");
     ui->gifLabel->setMovie(movie);
     movie->start();
 
