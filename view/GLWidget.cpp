a76cbee9b211f10f26eacfd1cb7a07c2f96bcd2f
ahaker <ahaker@uos.de>
2019-03-21 15:18:52 +0100

    doppelt keyspace entfernt

--- view/GLWidget.cpp
+++ view/GLWidget.cpp
@@ -226,33 +226,14 @@ void GLWidget::step(map<Qt::Key, bool>& keyStates)
                 auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
                 auto value = now_ms.time_since_epoch();
                 long bulletShot = value.count();
-                //Ermittelt, wann die letzte Kugel abgeschoßen wurde und erlaubt erst nach
-                if(bulletShot - m_lastBullet > m_schussFrequenz){
-
-                    //Lässt Bullet bei der Kanone des Raumschiffes erscheinen
-                    Vector3f shipPosition = m_actor->getPosition() + m_actor->getZAxis() * -45 + m_actor->getXAxis() * -175;
-                    Bullet::Ptr bullet = make_shared<Bullet>(Bullet(shipPosition, m_actor->getDirection()));
-                    m_physicsEngine->addBullet(bullet);
-                    m_lastBullet = bulletShot;
-                }
-                
-            }
-
-            // Add a bullet to physics engine
-            if(keyStates[Qt::Key_Space])
-            {
-                
-                auto now = std::chrono::system_clock::now();
-                auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
-                auto value = now_ms.time_since_epoch();
-                long bulletShot = value.count();
                 ///Ermittelt, wann die letzte Kugel abgeschoßen wurde und erlaubt erst nach
+                    std::cerr << "0bullet_shot: " << __LINE__ << "\n";
                 if(bulletShot - m_lastBullet > m_schussFrequenz){
+                    std::cerr << "bullet_shot: " << __LINE__ << "\n";
                     Bullet::Ptr bullet = make_shared<Bullet>(Bullet(m_actor->getPosition(), m_actor->getDirection()));
                     m_physicsEngine->addBullet(bullet);
                     m_lastBullet = bulletShot;
                     bullet_shot = Bullet_shot::shot;
-                    std::cerr << "bullet_shot: " << __LINE__ << "\n";
                 }
                 
             }
