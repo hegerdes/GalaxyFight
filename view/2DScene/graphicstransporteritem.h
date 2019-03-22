a59a20745c8a4de1f158765893ecb50802cd2450
lkreienbrink <lkreienbrink@uni-osnabrueck.de>
2019-03-22 15:37:48 +0100

    added comments

--- view/2DScene/graphicstransporteritem.h
+++ view/2DScene/graphicstransporteritem.h
@@ -7,7 +7,7 @@
 
 namespace asteroids {
 /**
- * @brief The GraphicsTransporterItem class
+ * @brief The GraphicsTransporterItem class represents a transporter
  * @author lkreienbrink
  */
 class GraphicsTransporterItem : public QGraphicsItem
@@ -17,13 +17,13 @@ public:
 
 
     /**
-     * @brief boundingRect
+     * @brief boundingRect the size of the graphicsItems
      * @return
      */
     QRectF boundingRect() const override;
 
     /**
-     * @brief paint
+     * @brief paint paints a transporter svg and a circle if the transporter is selected
      * @param painter
      * @param option
      * @param widget
@@ -31,10 +31,22 @@ public:
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
                    QWidget *widget) override;
 
+    /**
+     * @brief type returns the GraphicsItem typw
+     * @return The custom GraphicsItem ItemType for the transporter
+     */
     int type() const override { return ItemTypes::Transporter;}
 
+    /**
+     * @brief getID returns the transporter id
+     * @return transporter id
+     */
     int getID();
 
+    /**
+     * @brief selected activates or deactivates the selection.
+     * @param flag true if item is selected otherwise false
+     */
     void selected(bool flag = true);
 
 private:
