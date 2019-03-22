227f914f43f36495069a53bf329f2f23e59d0250
Lennart <lekaiser@uos.de>
2019-03-22 11:59:07 +0100

    Changed server structure

--- qtclient/client.h
+++ qtclient/client.h
@@ -5,60 +5,62 @@
 #include <QtNetwork>
 #include <iostream>
 #include <math/Vector.hpp>
-#include <qtserver/packettypes.h>
 #include <qtserver/flags.h>
+#include <qtserver/packettypes.h>
 #include <vector>
 
-namespace asteroids{
-    class Client
-    {
-        public:
-            Client();
+namespace asteroids {
+class Client {
+  public:
+    Client();
 
-            //init_3D
-            Vector<float> ownPos;
-            Vector<float> ownxAxis;
-            Vector<float> ownyAxis;
-            Vector<float> ownzAxis;
+    // init_3D
+    Vector<float> ownPos;
+    Vector<float> ownxAxis;
+    Vector<float> ownyAxis;
+    Vector<float> ownzAxis;
 
-            Vector3f pos_astr[10];
-            Vector3f dir_astr[10];
-            float size_astr[10];
-            int count_astr;
-            int id_astr[10];
+    Vector3f pos_astr[10];
+    Vector3f dir_astr[10];
+    float size_astr[10];
+    int count_astr;
+    int id_astr[10];
 
-            //update_3D_S / init_3D
-            Vector<float> enemyPos;
-            Vector<float> enemyxAxis;
-            Vector<float> enemyyAxis;
-            Vector<float> enemyzAxis;
-            Bullet_shot enemy_shot;
-            int enemy_shot_id;
+    // update_3D_S / init_3D
+    Vector<float> enemyPos;
+    Vector<float> enemyxAxis;
+    Vector<float> enemyyAxis;
+    Vector<float> enemyzAxis;
+    Bullet_shot enemy_shot;
+    int enemy_shot_id;
 
-            std::vector<int> asteroids_deleted;
-            std::vector<int> bullet_deleted;
-            Hit own_hit;
+    std::vector<int> asteroids_deleted;
+    std::vector<int> bullet_deleted;
+    Hit own_hit;
 
-            //end_3D
-            char winner_no;
-            //start_2D
-            void sendUpdate_3D_C(Vector<float> pos, Vector<float> xAxis, Vector<float> yAxis, Vector<float> zAxis, Bullet_shot shot, Living living, int bullet_id);
-            void readData();
-            void wait_for_readData();
+    // end_3D
+    char winner_no;
+    // start_2D
+    void sendUpdate_3D_C(Vector<float> pos, Vector<float> xAxis, Vector<float> yAxis, Vector<float> zAxis,
+                         Bullet_shot shot, Living living, int bullet_id);
+    void readData();
+    void wait_for_readData(int timeout);
 
-            void connect(QString addr, quint16 port);
-            bool init_received;
-    private:
-            void writeData(QByteArray const & data);
-            void init_3d(char *);
-            void update_3D_S(char *);
+    void connect(QString addr, quint16 port);
+    bool init_received;
 
-            QTcpSocket socket;
-            float getFloat(char **ptr);
-            int getInt(char **ptr);
-            char getChar(char **ptr);
-            short getShort(char **ptr);
-    };
-}
+  private:
+    void writeData(QByteArray const& data);
+    void init_3d(char*);
+    void update_3D_S(char*);
+
+    QTcpSocket socket;
+    float getFloat(char** ptr);
+    int getInt(char** ptr);
+    char getChar(char** ptr);
+    short getShort(char** ptr);
+    void interpreteAnswer();
+};
+} // namespace asteroids
 
 #endif // CLIENT_H
--- qtserver/mytcpserver.h
+++ qtserver/mytcpserver.h
@@ -1,54 +1,54 @@
 #ifndef MYTCPSERVER_H
 #define MYTCPSERVER_H
 
-#include <QObject>
-#include <QTcpSocket>
-#include <QTcpServer>
+#include "../math/Randomizer.hpp"
+#include "../math/Vector.hpp"
+#include "flags.h"
 #include <QDebug>
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
 #include <QtCore>
 #include <QtNetwork>
-#include "../math/Vector.hpp"
-#include "../math/Randomizer.hpp"
-#include "flags.h"
 
 #include "packettypes.h"
 
-class Server : public QObject
-{
+class Server : public QObject {
     Q_OBJECT
-public:
-    explicit Server(QObject *parent = 0);
+  public:
+    explicit Server(QObject* parent = 0);
 
-signals:
+  signals:
     bool dataReceived(QByteArray);
 
-public slots:
+  public slots:
     void newConnection();
     void disconnected();
     void readyRead();
-    bool writeData(QByteArray const &);
+    bool writeData(QByteArray const&);
 
-public:
+  public:
     QByteArray data;
-    QTcpServer *server;
-    QHash<QTcpSocket*, QByteArray*> buffers; //We need a buffer to store data until block has completely received
-    QHash<QTcpSocket*, qint32*> sizes; //We need to store the size to verify if a block has received completely
-    QByteArray test {"test_qstring"};
-    //unsigned int count {0};
-    //float count {0};
-    //float factor {1};
+    QTcpServer* server;
+    QHash<QTcpSocket*, QByteArray*> buffers; // We need a buffer to store data until block has completely
+                                             // received
+    QHash<QTcpSocket*, qint32*> sizes;       // We need to store the size to verify if
+                                             // a block has received completely
+    QByteArray test{"test_qstring"};
+    // unsigned int count {0};
+    // float count {0};
+    // float factor {1};
     float position_temp[12];
     float position_temp_2[12];
 
-    QTcpSocket *socket_1 = nullptr;
-    QTcpSocket *socket_2 = nullptr;
+    QTcpSocket* socket_1 = nullptr;
+    QTcpSocket* socket_2 = nullptr;
     float getFloat(char** ptr);
     short getShort(char** ptr);
     char getChar(char** ptr);
     int getInt(char** ptr);
 
-
-    struct client_data{
+    struct client_data {
         asteroids::Vector<float> position;
         asteroids::Vector<float> xAxis;
         asteroids::Vector<float> yAxis;
@@ -61,13 +61,17 @@ public:
     client_data user_data_1;
     client_data user_data_2;
 
-private:
-       bool already_send_1 = false;
-       bool already_send_2 = false;
+  private:
+    bool already_send_1 = false;
+    bool already_send_2 = false;
 
-       asteroids::Vector3f pos_astr[10];
-       asteroids::Vector3f dir_astr[10];
-       float size_astr[10];
+    asteroids::Vector3f pos_astr[10];
+    asteroids::Vector3f dir_astr[10];
+    float size_astr[10];
+    // sending packets
+    void sendInit_3D(QByteArray& response, QTcpSocket* socket);
+    void sendUpdate_3D_C(QByteArray& response, QTcpSocket* socket);
+    // receive packets
 };
 
 #endif // MYTCPSERVER_H
