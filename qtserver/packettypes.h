25b9b42898759c8d0cb987046854ae31a37bb012
ckehr <ckehr@uos.de>
2019-03-28 12:52:40 +0100

    addes comments

--- qtserver/packettypes.h
+++ qtserver/packettypes.h
@@ -1,6 +1,8 @@
 #ifndef PACKETTYPES_H
 #define PACKETTYPES_H
-
+/**
+ * @brief The PacketType enum contains each packageType so that the Client and the server can interpret every package
+ */
 enum PacketType { init_3D, update_3D_C, update_3D_S, end_3D, start_2D, ready_T, game_start, con_lost};
 
 #endif // PACKETTYPES_H
